80f3ca39b9afb1182093e9bd137bdf0d6b06c047
Henrique Manduca <henrique.manduca@externo.cresol.com.br>
2024-08-01 10:17:01 -0300

    remove comment

--- src/models/manga.py
+++ src/models/manga.py
@@ -5,7 +5,8 @@ from src.database.connection import Connection
 
 class Manga(Model):
     name = CharField()
+    directory = IntegerField()
     last_downloaded = IntegerField()
 
     class Meta:
-        database = Connection.get_db() # This model uses the "people.db" database.
+        database = Connection.get_db()
